1cec7c65f45ab73a18a413b81a3bf9069eb07510
Ralph Largo <devlargs@gmail.com>
2021-01-10 06:24:54 +0800

    feat: initial tailwind cdn with vue setup

--- @root/vue/src/App.vue
+++ @root/vue/src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav
+  },
+  data: () => {
+    return {
+      light: false
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+input[type="checkbox"]:checked + label span:first-of-type {
+  background-color: #10b981;
+  border-color: #10b981;
+  color: #fff;
 }
+
+input[type="checkbox"]:checked + label span:nth-of-type(2) {
+  text-decoration: line-through;
+  color: #9ca3af;
+}
+
+input[type="checkbox"]:checked + label input[type="text"] {
+  text-decoration: line-through;
+  color: #9ca3af;
+}
+
+.switch-off {
+  background-color: #111827;
+}
+
+.img-border {
+  border: 2px dashed #111827;
+  padding: 7px;
+}
+
+.home-title {
+  color: #111827 !important;
+}
+
+.home-body {
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%235c1cc1' fill-opacity='0.06' fill-rule='evenodd'/%3E%3C/svg%3E");
+}
+
+.main-container {
+  height: calc(100vh - 64px);
+}
+
+.logo-image {
+  cursor: pointer !important;
+}
+
+.list-container {
+  max-height: 40vh;
+  overflow-y: auto;
+}
+
+::-webkit-scrollbar-track {
+  border: 1px solid #1f2937;
+  background-color: #f5f5f5;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+  background-color: #f5f5f5;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #1f2937;
+}
+
+.list-scroll-dark::-webkit-scrollbar-track {
+  border: 1px solid lightblue !important;
+  background-color: #111827 !important;
+}
+
+.list-scroll-dark::-webkit-scrollbar {
+  width: 10px !important;
+  background-color: white !important;
+}
+
+.list-scroll-dark::-webkit-scrollbar-thumb {
+  background-color: #60a5fa !important;
+}
+
+.list-parent {
+  max-height: 60vh;
+}
+
+.delete-button:active {
+  color: lightcoral;
+  cursor: progress;
+}
+
 </style>
