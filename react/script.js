2f6fd12f71382666f6dcdbf63412e2f7bdd1805b
Ralph Largo <devlargs@gmail.com>
2021-01-09 12:48:42 +0800

    chore: run new build

--- react/script.js
+++ react/script.js
@@ -342,43 +342,6 @@ const Root = () => {
             ) : (
               <div />
             )}
-
-            <form>
-              <div className="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded">
-                <svg
-                  className="w-5 h-5 text-gray-400 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                  />
-                </svg>
-                <input
-                  required
-                  className="flex-grow h-8 ml-3 bg-transparent focus:outline-none font-medium"
-                  type="text"
-                  placeholder="Add a new task"
-                  value={addInput}
-                  onChange={(e) => setAddInput(e.target.value)}
-                />
-                <button
-                  className={`pl-2 pr-2 pt-1 pb-1 rounded-sm focus:outline-none ${
-                    light
-                      ? "hover:bg-gray-200 bg-gray-300 text-gray-900"
-                      : "hover:bg-gray-700 bg-gray-900"
-                  }`}
-                  onClick={addNewTask}
-                >
-                  Add
-                </button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
