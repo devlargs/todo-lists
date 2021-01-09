400c2a7960e7029bdf7a321de5f7a277e96d423c
Ralph Largo <devlargs@gmail.com>
2021-01-09 11:24:47 +0800

    feat: added toggle switch on header

--- react/script.js
+++ react/script.js
@@ -1,10 +1,5 @@
 const Root = () => {
-  const [light, setLight] = React.useState(false);
-  const [lists, setLists] = React.useState([
-    {
-      key: "ralphlargo",
-    },
-  ]);
+  const light = true;
 
   return (
     <div>
@@ -52,12 +47,11 @@ const Root = () => {
               </button>
             </div>
             <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div class="flex-shrink-0 flex items-center">
-                <img
-                  class="block lg:hidden h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="Workflow"
-                />
+              <div
+                class="flex-shrink-0 flex items-center logo-image"
+                onClick={() => (window.location.pathname = "/")}
+              >
+                <img class="block lg:hidden h-8 w-auto" src="" alt="Workflow" />
                 <img
                   class="hidden lg:block h-8 w-auto"
                   src="https://image-placeholder.com/images/actual-size/110x32.png"
@@ -70,32 +64,59 @@ const Root = () => {
                     href="#"
                     class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
                   >
-                    Dashboard
+                    React
                   </a>
                   <a
                     href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    class="text-white px-3 py-2 rounded-md text-sm font-small hover:bg-gray-300 hover:text-black"
                   >
-                    Team
+                    Angular
                   </a>
                   <a
                     href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    class="text-white px-3 py-2 rounded-md text-sm font-small hover:bg-gray-300 hover:text-black"
                   >
-                    Projects
+                    Svelte
                   </a>
                   <a
                     href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    class="text-white px-3 py-2 rounded-md text-sm font-small hover:bg-gray-300 hover:text-black"
                   >
-                    Calendar
+                    Vue
+                  </a>
+                  <a
+                    href="#"
+                    class="text-white px-3 py-2 rounded-md text-sm font-small hover:bg-gray-300 hover:text-black"
+                  >
+                    Ember
+                  </a>
+                  <a
+                    href="#"
+                    class="text-white px-3 py-2 rounded-md text-sm font-small hover:bg-gray-300 hover:text-black"
+                  >
+                    jQuery
+                  </a>
+                  <a
+                    href="#"
+                    class="text-white px-3 py-2 rounded-md text-sm font-small hover:bg-gray-300 hover:text-black"
+                  >
+                    Backbone
                   </a>
                 </div>
               </div>
             </div>
+
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                <span class="sr-only">View notifications</span>
+              {/* <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"> */}
+              <span class="border rounded-full border-grey flex items-center cursor-pointer w-12 justify-start bg-gray-300">
+                <span class="rounded-full border w-6 h-6 border-grey shadow-inner bg-blue-400 shadow"></span>
+              </span>
+
+              <span class="border rounded-full border-grey flex items-center cursor-pointer w-12 bg-black-100 justify-end">
+                <span class="rounded-full border w-6 h-6 border-grey shadow-inner bg-blue-400 white shadow"></span>
+              </span>
+
+              {/* <span class="sr-only">View notifications</span>
 
                 <svg
                   class="h-6 w-6"
@@ -111,8 +132,8 @@ const Root = () => {
                     stroke-width="2"
                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                   />
-                </svg>
-              </button>
+                </svg> */}
+              {/* </button> */}
 
               <div class="ml-3 relative">
                 <div>
@@ -121,66 +142,62 @@ const Root = () => {
                     id="user-menu"
                     aria-haspopup="true"
                   >
-                    <span class="sr-only">Open user menu</span>
+                    <span class="sr-only">Ralph Largo</span>
                     <img
                       class="h-8 w-8 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      src="https://largs.s3.amazonaws.com/2x2.jpg"
                       alt=""
                     />
                   </button>
-                </div>
-
-                <div
-                  class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="user-menu"
-                >
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                  >
-                    Your Profile
-                  </a>
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                    role="menuitem"
-                  >
-                    Settings
-                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="hidden sm:hidden">
+        <div class="sm:hidden">
           <div class="px-2 pt-2 pb-3 space-y-1">
             <a
               href="#"
               class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
             >
-              Dashboard
+              React
             </a>
             <a
               href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              class="text-white block px-3 py-2 rounded-md text-base font-small"
             >
-              Team
+              Angular
             </a>
             <a
               href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              class="text-white block px-3 py-2 rounded-md text-base font-small"
             >
-              Projects
+              Svelte
             </a>
             <a
               href="#"
-              class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              class="text-white block px-3 py-2 rounded-md text-base font-smallm"
             >
-              Calendar
+              Vue
+            </a>
+            <a
+              href="#"
+              class="text-white block px-3 py-2 rounded-md text-base font-small"
+            >
+              Ember
+            </a>
+            <a
+              href="#"
+              class="text-white block px-3 py-2 rounded-md text-base font-small"
+            >
+              jQeuery
+            </a>
+            <a
+              href="#"
+              class="text-white block px-3 py-2 rounded-md text-base font-small"
+            >
+              Backbone
             </a>
           </div>
         </div>
@@ -208,17 +225,6 @@ const Root = () => {
                 <h4 className="font-semibold ml-3 text-lg mr-3">
                   React Todo list
                 </h4>
-                <button
-                  type="button"
-                  aria-pressed="false"
-                  className="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  <span className="sr-only">Use setting</span>
-                  <span
-                    aria-hidden="true"
-                    className="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-                  ></span>
-                </button>
               </div>
 
               <div>
@@ -320,36 +326,7 @@ const Root = () => {
                 </svg>
                 <h4 className="font-semibold ml-3 text-lg">React Todo List</h4>
               </div>
-              <div>
-                <input
-                  className="hidden"
-                  type="checkbox"
-                  id="task_7"
-                  defaultChecked
-                />
-                <label
-                  className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900"
-                  htmlFor="task_7"
-                >
-                  <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full">
-                    <svg
-                      className="w-4 h-4 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </span>
-                  <span className="ml-4 text-sm">
-                    Eavesdrop on Master Frodo & Gandalf.
-                  </span>
-                </label>
-              </div>
+
               <div>
                 <input className="hidden" type="checkbox" id="task_10" />
                 <label
@@ -370,31 +347,42 @@ const Root = () => {
                       />
                     </svg>
                   </span>
-                  <span className="ml-4 text-sm">Be all round legend.</span>
+
+                  <input
+                    type="text"
+                    className="bg-transparent pl-4 text-sm focus:outline-none "
+                    defaultValue="Be all round legend"
+                  />
                 </label>
               </div>
 
-              <button className="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded">
-                <svg
-                  className="w-5 h-5 text-gray-400 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              <form>
+                <div className="flex items-center w-full h-8 px-2 mt-2 text-sm font-medium rounded">
+                  <svg
+                    className="w-5 h-5 text-gray-400 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                    />
+                  </svg>
+                  <input
+                    required
+                    className="flex-grow h-8 ml-3 bg-transparent focus:outline-none font-medium"
+                    type="text"
+                    placeholder="Add a new task"
                   />
-                </svg>
-                <input
-                  className="flex-grow h-8 ml-4 bg-transparent focus:outline-none font-medium"
-                  type="text"
-                  placeholder="add a new task"
-                />
-              </button>
+                  <button className="bg-gray-900 pl-2 pr-2 pt-1 pb-1 rounded-sm focus:outline-none hover:bg-gray-700">
+                    Add
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         )}
